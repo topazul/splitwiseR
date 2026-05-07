22bdc03068a7711f2a635eba4452473aef94f0af
Topaz Mohaban <topazul@gmail.com>
2026-05-07 16:30:33 +0300

    fix clsx

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,4 @@
-import { type ClassValue, clsx } from 'clsx'
-
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: (string | undefined | null | false)[]) {
   return inputs.filter(Boolean).join(' ')
 }
 
